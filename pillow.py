ffc0547e2a1391918d62a51076d6fa521eb10291
Hardik1172 <hardikpachwari@gmail.com>
2024-06-17 16:35:55 +0530

    fourth commit

--- pillow.py
+++ pillow.py
@@ -1,6 +1,10 @@
-a = input("input some comma separated numbers :")
-list = a.split(",")
-tuple = tuple(list)
-
-print("given list is :",list)
-print("given tuple is :", tuple)
+import math
+c= 50
+h =30
+d = input("enter numbers :")
+a = d.split(',')
+result_list=[]
+for numbers in d:
+    q = round(math.sqrt([2*c*d]/h))
+result_list.append(q)
+print(result_list)
