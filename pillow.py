8914c8b9b6fbeed99801dfdd26214aec2eb65b22
Hardik1172 <hardikpachwari@gmail.com>
2024-06-17 14:26:58 +0530

    first commit

--- pillow.py
+++ pillow.py
@@ -1,1 +1,12 @@
-from PIL import pillow
+from flask import Flask,jsonify,request
+app = Flask(__name__)
+@app.route('/', methods = ['Get','POST'])
+def home():
+    if(request.method == 'GET'):
+        data = "Hello World"
+        return jsonify({'data':data})
+    @app.route('/home/<int:num>', methods = ['GET'])
+    def disp(num):
+        return jsonify({'data: num**2'})
+    if __name__ == '__main__':
+        app.run(debug = True)
