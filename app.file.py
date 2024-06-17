278f6b0955f9346226a0e71ae63b6d9453478a4e
Hardik1172 <hardikpachwari@gmail.com>
2024-06-17 13:08:45 +0530

    pillow dev

--- app.file.py
+++ app.file.py
@@ -5,7 +5,7 @@ def hello():
    greet = '<h1>Hello, Gators!</h1>'
    link = '<p><a href="user/Albert">Click me!</a></p>'
    return greet + link
-@app.route('/user/<name>')
+@app.route('/user/<Nikunj''>')
 def user(name):
     personal = f'<h1>Hello, {name}!<h1>'
     instruc = '<p>Change the name in the <em> browser address bar</em> \
