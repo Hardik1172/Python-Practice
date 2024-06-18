e13ff8cfbfe8094c828f069c4c79722fd0cf9e12
Hardik <hardikpachwari@gmail.com>
2024-06-18 16:48:42 +0530

    sixth commit

--- pillow.py
+++ pillow.py
@@ -1,4 +1,15 @@
-def pangram():
-    list2 = []
-    for a in list1 range('a','z')
-    if
+from PIL import Image, ImageFont, ImageDraw
+img = Image.open(r"")
+img.show()
+draw = ImageDraw.Draw(img)
+draw.rectangle(xy=(50,50,150,150),
+               fill= (0,127,0),
+               outline=(255,255,255),
+               width = 5)
+draw.line(xy= (30,30,60,60),
+          fill= (0,127,0),
+          width= 40)
+draw.arc(xy= (50,50,80,80),
+         fill=(0,80,80),
+         outline=(60,60,50),
+         width=60)
