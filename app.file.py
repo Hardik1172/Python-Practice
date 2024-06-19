577859a997862dcd9ad7a10bd68145f77f74bc46
Hardik <hardikpachwari@gmail.com>
2024-06-19 11:57:58 +0530

    19 june first commit

--- app.file.py
+++ app.file.py
@@ -1,3 +1,11 @@
-list1 = [1,2,3,4,5,6]
-list2 = '-'.join(list1)
-print(list2)
+# Create a child class Bus that will inherit all of the variables and methods of the Vehicle class
+
+class vehicle:
+    def __init__(self, name,max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+class bus(vehicle):
+    pass
+School_bus = bus("school volvo", 180 , 12)
+print("vehicle name:", School_bus.name, "vehicle max_speed:", School_bus.max_speed, "mileage:", School_bus.mileage)
